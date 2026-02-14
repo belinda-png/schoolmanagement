d95a5727c613a58fd86a0b74006ac0b63169caa1
Mukhaye Belinda <namuzbelinda@gmail.com>
2026-02-14 20:37:19 +0300

    added a student model

--- management/models.py
+++ management/models.py
@@ -9,3 +9,13 @@ class Teacher(models.Model):
     class_name = models.CharField()
     def __str__(self):
         return self.Name
+    
+class Student(models.Model):
+    name = models.CharField(max_length=20)
+    age  = models.IntegerField()
+    grade = models.CharField(max_length=5)
+    cls = models.CharField(max_length=5)
+    class_teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE,)
+    def __str__(self):
+        return self.name    
+
--- management/views.py
+++ management/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from models import Teacher
+from flask import request
 # Create your views here.
+def teacher_list(request):
+        teachers = Teacher.objects.all()
+        return render(request, 'teacher_list.html', {'teachers': teachers})
